d89680296d8456aa9acd2a2f8c6e29c00cd03d3b
Kyle-Wisecarver <wiseworks.2022@gmail.com>
2022-03-30 13:28:11 -0500

    I will be attempting to use as little code as possible to sharpen my understanding of how Variables, Loops, Conditionals, Lists (no built-in List methods), Functions can be presented.

---
+++ main5.py
@@ -0,0 +1,19 @@
+# Problem Solving
+
+def my_function():
+    team = "Packers"
+    input = team(0)
+    art = team(5)
+    print(input + art)
+
+
+
+
+    
+    
+
+
+#create a function
+#take in a string
+#take the first and last charachter of the string
+#concatenate the characters    
